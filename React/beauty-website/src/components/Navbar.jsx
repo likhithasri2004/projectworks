0d0e859ff5573db6f7e68a04c2a20cfe34b63ef5
VarikalluLikhithaSri <varikalluramakrishna95@gmail.com>
2026-05-15 11:16:16 +0530

    home page

---
+++ React/beauty-website/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <a href="#">Home</a>
+      <a href="#">Products</a>
+      <a href="#">Services</a>
+      <a href="#">About Us</a>
+      <a href="#">Contact</a>
+    </nav>
+  )
+}
---
+++ React/beauty-website/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header'
+import Carousel from '../components/Carousel'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Carousel />
+      <Footer />
+    </>
+  )
+}
